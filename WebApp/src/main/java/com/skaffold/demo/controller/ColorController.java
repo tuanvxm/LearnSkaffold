20287845c0d2cad869cf023cdbd99174df3ebfb4
TuanVXM <TuanVXM@gmail.com>
2020-02-10 10:17:04 +0700

    Add checkRed api

--- WebApp/src/main/java/com/skaffold/demo/controller/ColorController.java
+++ WebApp/src/main/java/com/skaffold/demo/controller/ColorController.java
@@ -1,18 +1,20 @@
 package com.skaffold.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ColorController {
     @GetMapping(path = "/getRed")
     public String getRed() {
         return "Red";
     }
 
-    @GetMapping(path = "/getGreen")
-    public String getGreen() {
-        return "Green";
+    @GetMapping(path = "/checkRed")
+    public void checkRed() {
+
     }
 
     @GetMapping(path = "/getBlue")
